41deccff1cf7140cc7846dfd76b319cf4be4bcdc
Takondwa Kakusa <tk421@vt.edu>
2017-03-02 23:44:06 -0500

    added snapshot code

---
+++ object_movement_snap.py
@@ -0,0 +1,151 @@
+# USAGE
+# python object_movement.py --video object_tracking_example.mp4
+# python object_movement.py
+
+# import the necessary packages
+from collections import deque
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+def capture():
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video",
+            help="path to the (optional) video file")
+    ap.add_argument("-b", "--buffer", type=int, default=32,
+            help="max buffer size")
+    args = vars(ap.parse_args())
+
+    # define the lower and upper boundaries of the "green"
+    # ball in the HSV color space
+    greenLower = (29, 86, 6)
+    greenUpper = (64, 255, 255)
+
+    # initialize the list of tracked points, the frame counter,
+    # and the coordinate deltas
+    pts = deque(maxlen=args["buffer"])
+    counter = 0
+    (dX, dY) = (0, 0)
+    (X, Y) = (0,0)
+    direction = ""
+    radius = 0
+
+    # grab the reference to the webcam
+    camera = cv2.VideoCapture(0)
+
+    # grab the current frame
+    (grabbed, frame) = camera.read()
+
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    frame = imutils.resize(frame, width=600)
+    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+
+
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+
+    if radius > 0:
+        return True
+    else:
+        return False
+
+    # cleanup the camera and close any open windows
+    camera.release()
+    cv2.destroyAllWindows()
+
+def getLocation():
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video",
+            help="path to the (optional) video file")
+    ap.add_argument("-b", "--buffer", type=int, default=32,
+            help="max buffer size")
+    args = vars(ap.parse_args())
+
+    # define the lower and upper boundaries of the "green"
+    # ball in the HSV color space
+    greenLower = (29, 86, 6)
+    greenUpper = (64, 255, 255)
+
+    # initialize the list of tracked points, the frame counter,
+    # and the coordinate deltas
+    pts = deque(maxlen=args["buffer"])
+    counter = 0
+    (dX, dY) = (0, 0)
+    (x, y) = (0,0)
+    direction = ""
+    radius = 0
+
+    # grab the reference to the webcam
+    camera = cv2.VideoCapture(0)
+
+    # grab the current frame
+    (grabbed, frame) = camera.read()
+
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    frame = imutils.resize(frame, width=600)
+    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+
+
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+
+    if radius > 0:
+        return (x, y)
+    else:
+        return (-1,-1)
+
+    # cleanup the camera and close any open windows
+    camera.release()
+    cv2.destroyAllWindows()
+
+if capture():
+    print(getLocation())
